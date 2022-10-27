b470f9b2ca2d09d4129e4875ed0feac32a39bb1b
Zhiwei Cheng <zhiwei.cheng@uzh.ch>
2022-10-27 11:07:16 +0200

    #1 Add a new argument output_type to Kaya idententy calculation function, and add a unittest for the feature

--- test_Kaya.py
+++ test_Kaya.py
@@ -5,6 +5,7 @@ class TestKaya(unittest.TestCase):
 
     def test_multiply(self):
         self.assertEqual(kaya_identity(1,2,1,1).cal_kaya_identity(), 2.00)
+        self.assertEqual(kaya_identity(1,2,1,1,"C").cal_kaya_identity(), 0.53)
         self.assertEqual(kaya_identity(82.4,44,5,0.05).cal_kaya_identity(), 906.40)
         # self.assertEqual(kaya_identity("-82.4",44,5,0.05).cal_kaya_identity(), 906.40)
         # self.assertEqual(kaya_identity(-82.4,44,5,0.05).cal_kaya_identity(), 906.40)
